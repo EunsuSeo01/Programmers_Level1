1ca4704a94a0beb11a8efc5c9ecd41557c44d532
Eunsu Seo <87434861+EunsuSeo01@users.noreply.github.com>
2021-08-09 02:05:43 +0900

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+/*
+ Programmers Lv1 월간 코드 챌린지 시즌2 - 음양 더하기
+ Created by EunSu Seo
+ Last modified 2021-08-09
+*/
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (!signs[i])
+                absolutes[i] = -absolutes[i];
+            answer += absolutes[i];
+        }
+        
+        return answer;
+    }
+}
