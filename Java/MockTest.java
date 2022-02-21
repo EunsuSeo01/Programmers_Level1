ad3e3691702bfc7d5df0af915691153a7ccce691
EUNSU SEO <eunsu0613@naver.com>
2022-02-21 23:08:14 +0900

    Update MockTest.java

--- Java/MockTest.java
+++ Java/MockTest.java
@@ -1,3 +1,11 @@
+/*
+ Programmers 완전탐색 Lv 1. 모의고사 (java 풀이) - 서은수
+ 3명에겐 각각 자신만의 문제 푸는 패턴이 존재.
+ 1번 문제부터 마지막 문제까지의 정답이 순서대로 들어있는 배열 answers가 주어졌을 때,
+ 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해라.
+ 제한사항: 일등이 다수일 경우, 오름차순으로 정렬하여 return.
+*/
+
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
